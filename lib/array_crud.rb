aa4c235a0460ce02c31300077de5b0a3d96a6c04
James Rogers <jarogers095@gmail.com>
2020-01-26 09:14:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,12 +17,12 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array.pop()
+  array.pop
   return array
 end
 
 def remove_element_from_start_of_array(array)
-  array.shift()
+  array.shift
   return array
 end
 
@@ -31,9 +31,9 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  return array.first()
+  return array.first
 end
 
 def retrieve_last_element_from_array(array)
-  return array.last()
+  return array.last
 end
